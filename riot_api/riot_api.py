ac1b0c067d15218d9bc37c9e8ac548813ea3f99f
KyleRiggen <kyle.riggenbach@gmail.com>
2023-04-12 00:28:27 -0600

    starting on the actual matches

--- riot_api/riot_api.py
+++ riot_api/riot_api.py
@@ -1,8 +1,7 @@
 from riotwatcher import LolWatcher
 from dotenv import load_dotenv
+from filter_match_list import get_match_list
 import os
-import json
-import time
 
 def setup_enviorment():
   load_dotenv('../config2.env')
@@ -11,57 +10,19 @@ def setup_enviorment():
   del(api_key)
   return lol_watcher
 
-with open('champs.json') as user_file:
-  file_contents = user_file.read()
-champs_json = json.loads(file_contents)
-
-with open('puuid_list.json') as user_file:
-  file_contents = user_file.read()
-puuid_list_json = json.loads(file_contents)
-
 lol_watcher = setup_enviorment()
 player_region = 'na1'
 
-def get_match_list():
+match_list = get_match_list()
+print(match_list)
 
-  # config numbers for API calls
-  players_to_check = 3
-  matches_to_check = 3
+for match in match_list:
+  match_data = lol_watcher.match.by_id(region=player_region, match_id=match)['info']['participants']
+  count = 0
+  for player in match_data:
+    print(match_data[count]['championName'])
+    count = count + 1
 
-  # loop through the puuids to get a match list
-  match_list = []
-  for index, eachPlayer in enumerate(puuid_list_json):
-    match = lol_watcher.match.matchlist_by_puuid(region=player_region, puuid=eachPlayer, queue=420, start=0, count=matches_to_check)
-    print(index, match)
-    for eachMatch in match:
-      match_list.append(eachMatch)
-    if (index + 1) >= players_to_check:
-      break
-
-  print(f'starting amount: {len(match_list)}')
-
-  # remove duplicates
-  match_list = list(dict.fromkeys(match_list))
-
-  # remove old matches
-  now = time.time()
-  one_day = 86400
-  one_day_ago = now - one_day
-  new_match_list = []
-
-  for match in match_list:
-    match_timestamp = lol_watcher.match.by_id(region=player_region, match_id=match)['info']['gameStartTimestamp']
-    converted_time = match_timestamp / 1000
-    if converted_time > one_day_ago:
-      new_match_list.append(match)
-      print(f'adding: {time.ctime(converted_time)} < {time.ctime(one_day_ago)}')
-
-  print(f'ending amount: {len(new_match_list)}')
-
-  return new_match_list
-
-finished_match_list = get_match_list()
-print(finished_match_list)
 
 
 
