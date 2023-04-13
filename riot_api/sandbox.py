78c23647a7dde4b47e9f1fbfa1f40b79ff8c7730
KyleRiggen <kyle.riggenbach@gmail.com>
2023-04-13 13:54:10 -0600

    finished product raw product

--- riot_api/sandbox.py
+++ riot_api/sandbox.py
@@ -1,16 +1,46 @@
-# removing duplicated from the list using naive methods
+match_list = [12345, 23456, 34567]
 
-# initializing list
-sam_list = [11, 13, 15, 16, 13, 15, 16, 11]
-print ("The list is: " + str(sam_list))
+thing = {
+    'matchID': 12345,
+    'startTime': 1681261870233,
+    'players': [
+        {
+            'player1': 'john',
+            'champ': 'champ1'
+        },
+        {
+            'player2': 'bill',
+            'champ': 'champ2'
+        }
+    ]
+}
 
-# remove duplicated from list
-result = []
-for i in sam_list:
-    if i not in result:
-        result.append(i)
-    else:
-        print(f'removed: {i}')
+final_dictionary = {
+    {'matchID': 12345,
+     'startTime': 1681261870233,
+     'players': [
+         {
+             'player1': 'john',
+             'champ': 'champ1'
+         },
+         {
+             'player2': 'bill',
+             'champ': 'champ2'
+         }
+     ]
+     },
+    {'matchID': 12345,
+     'startTime': 1681261870233,
+     'players': [
+         {
+             'player1': 'john',
+             'champ': 'champ1'
+         },
+         {
+             'player2': 'bill',
+             'champ': 'champ2'
+         }
+     ]
+     }
+}
 
-# printing list after removal
-print ("The list after removing duplicates : " + str(result))
--- riot_api/try2_match_filter.py
+++ riot_api/try2_match_filter.py
@@ -9,16 +9,13 @@ with open('json/puuid_list.json') as user_file:
     file_contents = user_file.read()
 puuid_list_json = json.loads(file_contents)
 
-print(puuid_list_json)
-print(len(puuid_list_json))
-
 match_list = []
 
 for index, player in enumerate(puuid_list_json):
     matches = lol_watcher.match.matchlist_by_puuid(region=player_region, puuid=player, queue=420, start=0, count=5)
-    if index >= 1:
+    if index >= 4:
         break
-    print(index, matches)
+    print(index, matches, player)
     for match in matches:
         match_list.append(match)
 
@@ -34,8 +31,53 @@ for match in match_list:
 print(result)
 print(f'match list number:{len(result)}')
 
+all_matches = []
+
 for match in result:
-    match_info = lol_watcher.match.by_id(region=player_region, match_id=match)['info']
-    match_timestamp = match_info['gameStartTimestamp']
-    converted_time = match_timestamp/1000
-    print(time.ctime(converted_time))
+    match_info = lol_watcher.match.by_id(region=player_region, match_id=match)
+    match_timestamp = match_info['info']['gameStartTimestamp']
+    converted_time = match_timestamp / 1000
+
+    player_list = []
+    for player in match_info['info']['participants']:
+        player_dictionary = {
+            'puuid': player['puuid'],
+            'summonerName': player['summonerName'],
+            'champID': player['championId'],
+            'champName': player['championName'],
+            'kills': player['kills'],
+            'deaths': player['deaths'],
+            'assists': player['kills'],
+            'win': player['win']
+        }
+
+        # Add player dictionary to our player_list
+        player_list.append(player_dictionary)
+
+    match_dictionary = {
+        'matchId': match_info['metadata']['matchId'],
+        'gameStartTimestamp': converted_time,
+        'players': player_list
+    }
+
+    all_matches.append(match_dictionary)
+
+print(all_matches)
+print(len(all_matches))
+
+now = time.time()
+one_day = 86400
+one_day_ago = now - one_day
+all_matches_timed = []
+
+for eachMatch in all_matches:
+    viewableTime = time.ctime(eachMatch['gameStartTimestamp'])
+    rawTime = eachMatch['gameStartTimestamp']
+    if rawTime > one_day_ago:
+        all_matches_timed.append(eachMatch)
+        print(f'added: {viewableTime}')
+    else:
+        print(f'removed: {viewableTime}')
+
+print(all_matches_timed)
+print(len(all_matches_timed))
