626d8b0be8e0578aee31017709b1742b16d2d836
KyleRiggen <kyle.riggenbach@gmail.com>
2023-05-06 12:30:20 -0600

    starting on MSI stats

---
+++ msi2023/sandbox.py
@@ -0,0 +1,87 @@
+match = {
+    "winner": "G2",
+    "looser": "LOUD",
+    "bans": ['Xayah', 'Olaf', 'Maokai', 'Blitzcrank', 'Leona', 'Draven', 'Kennen', 'Annie', 'Jayce', 'Thresh'],
+    'picks': [
+        {
+            'champ': 'Darius',
+            'win': True,
+            'kills': 12,
+            'deaths': 3,
+            'assists': 9,
+            'position': 'top'
+        },
+        {
+            'champ': 'Nidalee',
+            'win': True,
+            'kills': 8,
+            'deaths': 2,
+            'assists': 15,
+            'position': 'jungle'
+        },
+        {
+            'champ': 'Nautilus',
+            'win': True,
+            'kills': 1,
+            'deaths': 3,
+            'assists': 20,
+            'position': 'mid'
+        },
+        {
+            'champ': 'Jinx',
+            'win': True,
+            'kills': 5,
+            'deaths': 3,
+            'assists': 13,
+            'position': 'bot'
+        },
+        {
+            'champ': 'Rell',
+            'win': True,
+            'kills': 0,
+            'deaths': 6,
+            'assists': 14,
+            'position': 'sup'
+        },
+        {
+            'champ': 'Renekton',
+            'win': False,
+            'kills': 2,
+            'deaths': 10,
+            'assists': 8,
+            'position': 'top'
+        },
+        {
+            'champ': 'Sejuani',
+            'win': False,
+            'kills': 3,
+            'deaths': 4,
+            'assists': 11,
+            'position': 'jungle'
+        },
+        {
+            'champ': 'Ahri',
+            'win': False,
+            'kills': 7,
+            'deaths': 5,
+            'assists': 7,
+            'position': 'mid'
+        },
+        {
+            'champ': 'Aphelios',
+            'win': False,
+            'kills': 5,
+            'deaths': 2,
+            'assists': 6,
+            'position': 'bot'
+        },
+        {
+            'champ': 'Tahm Kench',
+            'win': False,
+            'kills': 0,
+            'deaths': 5,
+            'assists': 6,
+            'position': 'sup'
+        }
+    ]
+}
